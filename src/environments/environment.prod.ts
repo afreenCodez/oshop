6d77bd601bb4540481bceb212ed04cff6d415c88
Your Name <you@example.com>
2019-02-26 00:39:23 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase:{
+  apiKey: "AIzaSyCu6rl1xNufKdJYLyiTG_Zw0hiiSmc5-38",
+    authDomain: "oshop-29a52.firebaseapp.com",
+    databaseURL: "https://oshop-29a52.firebaseio.com",
+    projectId: "oshop-29a52",
+    storageBucket: "oshop-29a52.appspot.com",
+    messagingSenderId: "194814632381"}
+};
